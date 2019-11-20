2b7702ab92e4e0d189fbb7939faffb2af5887b2b
Hari Kiran Yalavarthi <harikiranyalavarthi@qasymphony.com>
2019-11-20 14:25:44 -0500

    Ablility to get player profiles by searching using team names or player names

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # WebScrapingAPI
-A Node.js REST API for retrieving information using Web Scraping Technique
+A Node.js REST API for retrieving Cricket players statatics using Web Scraping Technique on ESPNCricinfo website.
+
+Sample Cricket Player Profile URL --> http://www.espncricinfo.com/india/content/player/28081.html
+
+Live Cricket Scores URL --> http://static.cricinfo.com/rss/livescores.xml
--- app.js
+++ app.js
@@ -3,62 +3,63 @@ const cheerio = require('cheerio');
 const tabletojson = require('tabletojson');
 const MongoClient = require('mongodb').MongoClient;
 
+const mongoDBURL = "mongodb://localhost:27017/espndb";
+
 let playerProfilesArray = [];
 let playerBattingArray = [];
 let playerBowlingArray = [];
 
-let resultPlayerId;
+const search_text = "dhoni";
 
-let getPlayersData = async function(playerProfilesArray, playerBattingArray, playerBowlingArray) {
-    for(let number=28081; number<28085; number++) {
+let getPlayersData = async function() {
+    for(let number=28081; number<28082; number++) {
         try {
             let response = await axios.get('http://www.espncricinfo.com/ci/content/player/'+number+'.html');
             if(response.status === 200) {
                 
-                let json = {playerId: number};
-                
+                let json = {player_id: number};
                 let $ = cheerio.load(response.data);
-
                 let info = $('.ciPlayerinformationtxt');
-
                 let numBasicInfo = $(info).get().length;
 
                 for(let index = 0; index < numBasicInfo; index++) {
                     let label = $(info).get(index).children[0].children[0].data.trim();
                     if (label === 'Full name') {
-                        json.fullName = $(info).get(index).children[1].next.children[0].data.trim();
+                        json.full_name = $(info).get(index).children[1].next.children[0].data.trim();
                     } else if (label === 'Born') {
-                        json.dob = $(info).get(index).children[1].next.children[0].data.trim();
+                        json.date_of_birth = $(info).get(index).children[1].next.children[0].data.trim();
                     } else if (label === 'Current age') {
-                        json.age = $(info).get(index).children[1].next.children[0].data.trim();
+                        json.current_age = $(info).get(index).children[1].next.children[0].data.trim();
                     } else if (label === 'Major teams') {
                         let teamsChildren = $(info).get(index).children;
                         let teams = "";
-                        for (let i = 0; i <= teamsChildren.length; i++) {
-                            if(i % 2 === 1 && i !== teamsChildren.length) {
-                                teams = teams + teamsChildren[i].next.children[0].data.trim() + " ";
+                        for (let a=0; a<=teamsChildren.length; a++) {
+                            if(a%2 === 1 && a !== teamsChildren.length) {
+                                teams = teams + teamsChildren[a].next.children[0].data.trim() + " ";
                             }
                         } 
-                        json.teams = teams.trim();
-                    } else if (label === 'Nickname') {
+                        json.major_teams = teams.trim();
+                    } else if (label === 'Nickname' || label === 'Also known as') {
                         let nicknamesChildren = $(info).get(index).children;
                         let nicknames = "";
-                        for (let i = 0; i <= nicknamesChildren.length; i++) {
-                            if(i % 2 === 1 && i !== nicknamesChildren.length) {
-                                nicknames = nicknames + nicknamesChildren[i].next.children[0].data.trim() + " ";
+                        for (let b=0; b<=nicknamesChildren.length; b++) {
+                            if(b%2 === 1 && b !== nicknamesChildren.length) {
+                                nicknames = nicknames + nicknamesChildren[b].next.children[0].data.trim() + " ";
                             }
                         }
                         json.nicknames = nicknames.trim(); 
                     } else if (label === 'Playing role') {
-                        json.role = $(info).get(index).children[1].next.children[0].data.trim();
+                        json.playing_role = $(info).get(index).children[1].next.children[0].data.trim();
                     } else if (label === 'Batting style') {
-                        json.batting = $(info).get(index).children[1].next.children[0].data.trim();
+                        json.batting_style = $(info).get(index).children[1].next.children[0].data.trim();
                     } else if (label === 'Bowling style') {
-                        json.bowling = $(info).get(index).children[1].next.children[0].data.trim();
+                        json.bowling_style = $(info).get(index).children[1].next.children[0].data.trim();
                     } else if (label === 'Height') {
                         json.playerHeight = $(info).get(index).children[1].next.children[0].data.trim();
                     } else if (label === 'Education') {
                         json.education = $(info).get(index).children[1].next.children[0].data.trim();
+                    } else if (label === 'Fielding position') {
+                        json.position = $(info).get(index).children[1].next.children[0].data.trim();
                     } else {
                         continue;
                     }
@@ -66,22 +67,21 @@ let getPlayersData = async function(playerProfilesArray, playerBattingArray, pla
 
                 playerProfilesArray.push(json);
 
-                tabletojson.convertUrl('http://www.espncricinfo.com/ci/content/player/'+number+'.html', function(tablesAsJson) {
-                    if(typeof tablesAsJson[0] !== 'undefined') {
-                        for(let k=0; k<tablesAsJson[0].length; k++) {
-                            if(tablesAsJson[0][k]['0'] === 'Tests' || 'ODIs' || 'T20Is' || 'First-class' || 'List A' || 'T20') {
-                                tablesAsJson[0][k].playerId = number;
-                                playerBattingArray.push(tablesAsJson[0][k]);
-                            }
-                        }
-                        for(let k=0; k<tablesAsJson[1].length; k++) {
-                            if(tablesAsJson[1][k]['0'] === 'Tests' || 'ODIs' || 'T20Is' || 'First-class' || 'List A' || 'T20') {
-                                tablesAsJson[1][k].playerId = number;
-                                playerBowlingArray.push(tablesAsJson[1][k]);
-                            }
+                const tablesAsJson = tabletojson.convert(response.data);
+                if(typeof tablesAsJson[0] !== 'undefined') {
+                    for(let j=0; j<tablesAsJson[0].length; j++) {
+                        if(tablesAsJson[0][j]['0'] === 'Tests' || 'ODIs' || 'T20Is' || 'First-class' || 'List A' || 'T20s') {
+                            tablesAsJson[0][j].player_id = number;
+                            playerBattingArray.push(tablesAsJson[0][j]);
                         }
                     }
-                });
+                    for(let k=0; k<tablesAsJson[1].length; k++) {
+                        if(tablesAsJson[1][k]['0'] === 'Tests' || 'ODIs' || 'T20Is' || 'First-class' || 'List A' || 'T20s') {
+                            tablesAsJson[1][k].player_id = number;
+                            playerBowlingArray.push(tablesAsJson[1][k]);
+                        }
+                    }
+                }
             }
         }   
         catch(error) {
@@ -96,64 +96,70 @@ let getPlayersData = async function(playerProfilesArray, playerBattingArray, pla
     }
 }
 
-getPlayersData(playerProfilesArray, playerBattingArray, playerBowlingArray)
-    .then(function() {
-        MongoClient.connect("mongodb://localhost:27017/cricDB", { useNewUrlParser: true }, (err, client) => {
-            if(!err) {
-                const db = client.db();
-                const profilesCollection = db.collection('playerProfiles');
-                const battingCollection = db.collection('playerBattingInfo');
-                const bowlingCollection = db.collection('playerBowlingInfo');
+getPlayersData().then(function() {
+    MongoClient.connect(mongoDBURL, { useNewUrlParser: true }, (err, client) => {
+        if(!err) {
+            const db = client.db();
+            const profilesCollection = db.collection('playerProfiles');
+            const battingCollection = db.collection('playerBattingInfo');
+            const bowlingCollection = db.collection('playerBowlingInfo');
 
-                // profilesCollection.insertMany(playerProfilesArray, function(err, result) {
-                //     if(!err) {
-                //         console.log(result.insertedCount);
-                //     } else {
-                //         console.log(err);
-                //     }
-                // });
-
-                // battingCollection.insertMany(playerBattingArray, function(err, result) {
-                //     if(!err) {
-                //         console.log(result.insertedCount);
-                //     } else {
-                //         console.log(err);
-                //     }
-                // });
-
-                // bowlingCollection.insertMany(playerBowlingArray, function(err, result) {
-                //     if(!err) {
-                //         console.log(result.insertedCount);
-                //     } else {
-                //         console.log(err);
-                //     }
-                // });
-
-                let getPlayer = function(callback) {
-                    profilesCollection.createIndex( { fullName: "text", teams: "text" } );
-                    profilesCollection.find({$text: { $search: "punjab"}}).toArray(function(err, result) {
-                        callback(result[0].playerId);
-                    });
+            profilesCollection.insertMany(playerProfilesArray, function(err, result) {
+                if(!err) {
+                    console.log("Inserted "+result.insertedCount+" player profile.");
+                } else {
+                    console.log(err);
                 }
+            });
 
-                getPlayer(function(resultId) {
-                    MongoClient.connect("mongodb://localhost:27017/cricDB", { useNewUrlParser: true }, (err, client) => {
-                        if(!err) {
-                            const db = client.db();
-                            const batCollection = db.collection('playerBattingInfo');
-                            batCollection.find({playerId: resultId}).toArray(function(err, result) {
+            battingCollection.insertMany(playerBattingArray, function(err, result) {
+                if(!err) {
+                    console.log("Inserted "+result.insertedCount+" batting records.");
+                } else {
+                    console.log(err);
+                }
+            });
+
+            bowlingCollection.insertMany(playerBowlingArray, function(err, result) {
+                if(!err) {
+                    console.log("Inserted "+result.insertedCount+" bowling records.");
+                } else {
+                    console.log(err);
+                }
+            });
+
+            let getPlayer = function(callback) {
+                profilesCollection.createIndex( { full_name: "text", major_teams: "text" } );
+                profilesCollection.find({$text: { $search: search_text}}).toArray(function(err, resultArray) {
+                    if(resultArray.length != 0) {
+                        console.log("Player found for search text "+search_text);
+                        console.log(resultArray);
+                        callback(resultArray);
+                    } else {
+                        console.log("No player found for search text "+search_text);
+                    }
+                });
+            }
+
+            getPlayer(function(resultArray) {
+                MongoClient.connect(mongoDBURL, { useNewUrlParser: true }, (err, client) => {
+                    if(!err) {
+                        const db = client.db();
+                        const batCollection = db.collection('playerBattingInfo');
+                        for(let l=0; l<resultArray.length; l++) {
+                            batCollection.find({player_id: resultArray[l].player_id}).toArray(function(err, result) {
                                 if(!err) {
                                     console.log(result);
                                 } else {
                                     console.log(err);
                                 }
                             });
-                        }
-                        client.close();
-                    });
+                        } 
+                    }
+                    client.close();
                 });
-            }
-            client.close();
-        });
-        
-    });
+            });
+        }
+        client.close();
+    });         
+});
