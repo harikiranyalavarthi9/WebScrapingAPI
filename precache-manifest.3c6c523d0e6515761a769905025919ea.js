10618f80f311e61ab3b467fb9aa41faa87132777
Hari Kiran Yalavarthi <harikiranyalavarthi@qasymphony.com>
2019-12-06 16:30:18 -0500

    Updates

---
+++ precache-manifest.3c6c523d0e6515761a769905025919ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e857e0448f6f06e802edf8af96a2b99",
+    "url": "/WebScrapingAPI/index.html"
+  },
+  {
+    "revision": "30ba3245f69eac101090",
+    "url": "/WebScrapingAPI/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "44612273a3f491271c79",
+    "url": "/WebScrapingAPI/static/css/main.35a4dc72.chunk.css"
+  },
+  {
+    "revision": "30ba3245f69eac101090",
+    "url": "/WebScrapingAPI/static/js/2.01cdb608.chunk.js"
+  },
+  {
+    "revision": "44612273a3f491271c79",
+    "url": "/WebScrapingAPI/static/js/main.7eb486a6.chunk.js"
+  },
+  {
+    "revision": "8773c76d15cd321aa520",
+    "url": "/WebScrapingAPI/static/js/runtime-main.a9b16831.js"
+  }
+]);
